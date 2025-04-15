cb9baaf584538d16b883d263a367f4301dfd869f
rizal <mrsetia1@gmail.com>
2025-04-15 17:22:09 +0700

    w/o sdk

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: isProduction ? '' : '',
+  assetPrefix: isProduction ? '/' : '',
+  trailingSlash: true
 };
 
 export default nextConfig;
